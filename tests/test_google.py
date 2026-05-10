23245f3b82e3585691d2813e2cd02a3729523fda
Atul Kamble <atul_kamble@hotmail.com>
2026-05-10 15:41:08 +0530

    Azure Test Plans Project

---
+++ tests/test_google.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+def test_google_title():
+
+    service = Service(ChromeDriverManager().install())
+
+    driver = webdriver.Chrome(service=service)
+
+    driver.get("https://www.google.com")
+
+    assert "Google" in driver.title
+
+    driver.quit()
